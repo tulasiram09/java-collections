593c710c03816095ea6884b61f8fe5b4fe651cb3
tulasiram09 <ponnagantitulasiram@gmail.com>
2024-11-18 10:31:05 +0530

    folder intialization

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class hello{
+    public static void main(String args[]){
+        System.out.println("Hello world!!!");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x*x);
+    }
+}
