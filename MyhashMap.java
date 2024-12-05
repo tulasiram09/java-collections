99dc97a061521a1214967e702bb31f7ea17f4221
tulasiram09 <ponnagantitulasiram@gmail.com>
2024-12-05 19:02:09 +0530

    hashmap

--- MyhashMap.java
+++ MyhashMap.java
@@ -1,5 +1,6 @@
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class MyhashMap {
     public static void main(String[] args) {
@@ -10,6 +11,17 @@ public class MyhashMap {
         h1.put(2,"Two");
         h1.put(3,"Three");
         h1.put(4,"Four");
+        System.out.println(h1);
+        h1.put(05,"Five"); // appends to the hashMap with key value pair
+        System.out.println(h1);
+        String Number1 = h1.get(2); // get the value associated with key 2
+        System.out.println(Number1);
+        System.out.println(h1.get(67)); 
+        System.out.println(h1.containsKey(5)); // Returns True of False checking the map if key is present or not
+        System.out.println(h1.containsKey(89));
+        Set <Integer> keys = h1.keySet();
+        System.out.println(keys);
+        h1.remove(2); // removes a key:value pair from the map
         System.out.println(h1);
     }
 }
